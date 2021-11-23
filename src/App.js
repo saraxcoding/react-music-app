74cf45f75f33d51bea2c8f341faf4cabf003977c
sara_h <smoke_screen16@hotmail.com>
2021-11-22 21:31:47 -0500

    Setting place holders for songs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import {useState} from 'react';
 
 function App() {
-  const [songs, setSongs] = useState{[
-
-  ]}
+  const [songs, setSongs] = useState([
+    {
+      title: "Song 1", /* place holders */
+      artist: "Artist 1"
+      img_src: "./images/song-1.jpg",
+      src: "./music/song1.mp3"
+    },
+    {
+      title: "Song 2",
+      artist: "Artist 2"
+      img_src: "./images/song-2.jpg",
+      src: "./music/song2.mp3"
+    },
+    {
+      title: "Song 3",
+      artist: "Artist 3"
+      img_src: "./images/song-3.jpg",
+      src: "./music/song3.mp3"
+    },
+  ])
   
   return (
     <div className="App">
