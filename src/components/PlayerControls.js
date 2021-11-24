bac26040f2431512f068701c3fb75033c830ae53
sara_h <smoke_screen16@hotmail.com>
2021-11-23 23:20:20 -0500

    Added player control icons

--- src/components/PlayerControls.js
+++ src/components/PlayerControls.js
@@ -1,9 +1,22 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import { faPlay, faPause, faForward, faBackward } from '@fortawesome/free-solid-svg-icons';
 
 const PlayerControls = () => {
     return (
-        <div>
+        <div className="c-player--controls">
+            <button className="skip-btn">
+                <FontAwesomeIcon icon={faBackward} />
+            </button>
+            <button className="play-btn">
+                <FontAwesomeIcon icon={faPlay} />
+            </button>
+            <button className="skip-btn">
+                <FontAwesomeIcon icon={faForward} />
+            </button>
 
         </div>
     )
 }
+
+export default PlayerControls;
